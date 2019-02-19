6cfa0975c840e32f2d45041c2787038e46c5d587
Jeroen Haisma <J_Haisma@hotmail.com>
2019-02-19 16:30:53 -0300

    listings and like button added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :homes, only: [ :index, :show, :destroy, :new, :create, :edit, :update ]
+  resources :homes, only: [ :index, :show, :destroy, :new, :create, :edit, :update ] do
+    resources :listings, only: [:create]
+  end
 
   root to: 'pages#home'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
