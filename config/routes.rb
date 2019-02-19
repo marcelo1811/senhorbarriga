22456b468a67f3874568b6268d8015d26d9319ab
rafael veenstra <rafaelveenstra@gmail.com>
2019-02-19 14:40:59 -0300

    created user show, still need to finish home div

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
 
   resources :homes, only: [ :index, :show, :destroy ]
+  resources :users, only: [ :show ]
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
