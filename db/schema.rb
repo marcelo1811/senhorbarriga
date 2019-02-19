9de80aaf224519b4be1afae0a9e679095d754ac7
Marcelo Miyachi <marcelo.m1811@gmail.com>
2019-02-19 10:38:13 -0300

    destroy method added, seed updated, show updated, title column added to home

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_220043) do
+ActiveRecord::Schema.define(version: 2019_02_19_011100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,11 +21,10 @@ ActiveRecord::Schema.define(version: 2019_02_18_220043) do
     t.integer "bed_available"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
     t.bigint "owner_id"
     t.string "description"
+    t.string "title"
     t.index ["owner_id"], name: "index_homes_on_owner_id"
-    t.index ["user_id"], name: "index_homes_on_user_id"
   end
 
   create_table "listings", force: :cascade do |t|
@@ -51,7 +50,6 @@ ActiveRecord::Schema.define(version: 2019_02_18_220043) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "homes", "users"
   add_foreign_key "homes", "users", column: "owner_id"
   add_foreign_key "listings", "homes"
   add_foreign_key "listings", "users", column: "student_id"
