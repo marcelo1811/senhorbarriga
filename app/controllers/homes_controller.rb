b653306bdeda3d017bb065bd7b471bdff69f65ce
Marcelo Miyachi <marcelo.m1811@gmail.com>
2019-02-21 21:19:03 -0300

    request.location at homes controller

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -14,16 +14,17 @@ class HomesController < ApplicationController
     @homes = policy_scope(Home).order(created_at: :desc)
     location = params[:location]
     max_dist = params[:max_dist].to_i
+    max_dist = 1000000 if max_dist = 0
     if location.nil? == false && location != ""
       if Geocoder.search(location).first.nil? == false
         lat = Geocoder.search(location).first.boundingbox[0].to_f
         lng = Geocoder.search(location).first.boundingbox[2].to_f
-        search = { lng: lng, lat: lat, home: false }
-        search_a = [lat, lng]
+        coordinates_hash = { lng: lng, lat: lat, home: false }
+        coordinates_array = [lat, lng]
       end
     end
 
-    @mark_homes = Home.where.not(latitude: nil, longitude: nil).near(search_a, max_dist)
+    @mark_homes = Home.where.not(latitude: nil, longitude: nil).near(coordinates_array, max_dist)
 
     @markers = @mark_homes.map do |home|
       {
@@ -37,23 +38,24 @@ class HomesController < ApplicationController
     end
 
     if location.nil? == false && location != ""
-      @markers << search if Geocoder.search(location).first.nil? == false
+      @markers << coordinates_hash if Geocoder.search(location).first.nil? == false
+    else
+      @markers << { lng: request.location.longitude.to_f, lat: request.location.latitude.to_f }
     end
 
-    if filter?(location, max_dist) == false
-      @mark_homes = @homes
-      @markers = @mark_homes.map do |home|
-      {
-        lng: home.longitude,
-        lat: home.latitude,
-        infoWindow: home.address,
-        home_description: home.description,
-        home_link: home_path(home),
-        home: true
-      }
-      end
-    end
-
+    # if filter?(location, max_dist) == false
+    #   @mark_homes = @homes
+    #   @markers = @mark_homes.map do |home|
+    #   {
+    #     lng: home.longitude,
+    #     lat: home.latitude,
+    #     infoWindow: home.address,
+    #     home_description: home.description,
+    #     home_link: home_path(home),
+    #     home: true
+    #   }
+    #   end
+    # end
   end
 
   def show
